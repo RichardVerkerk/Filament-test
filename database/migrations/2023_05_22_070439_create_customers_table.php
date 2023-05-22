758eb3628eafc4ae4aa22d7865a08819671d069f
Richard <riverk@gmail.com>
2023-05-22 09:49:19 +0200

    Create 2023_05_22_070439_create_customers_table.php
    Create customer table

---
+++ database/migrations/2023_05_22_070439_create_customers_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('address_line_1');
+            $table->string('address_line_2')->nullable();
+            $table->string('postalcode');
+            $table->string('city');
+            $table->string('country');
+            $table->string('telephone_1');
+            $table->string('telephone_2')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
